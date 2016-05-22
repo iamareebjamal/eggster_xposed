e2411d46fdd89dceb926a8163605e5f067668a49
iamareebjamal <jamal.areeb@gmail.com>
2016-05-22 15:56:03 +0530

    Reverted to original GB and HC PlatLogo

--- app/src/main/java/areeb/xposed/eggster/eggs/gb/PlatLogoActivity.java
+++ app/src/main/java/areeb/xposed/eggster/eggs/gb/PlatLogoActivity.java
@@ -1,85 +1,27 @@
 package areeb.xposed.eggster.eggs.gb;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 import areeb.xposed.eggster.R;
-import areeb.xposed.eggster.R.style;
 
-@SuppressLint("ShowToast")
 public class PlatLogoActivity extends Activity {
-    private Toast mToast;
+    Toast mToast;
 
-
-    @SuppressLint({"ShowToast", "InlinedApi", "NewApi"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        mToast = Toast.makeText(this, "Zombie art by Jack Larson", Toast.LENGTH_SHORT);
 
-
-        SharedPreferences pref = getSharedPreferences("preferenceggs", Context.MODE_PRIVATE);
-
-
-        String gbToast = pref.getString("gb_toast_text", getString(R.string.pref_default_gb_text));
-        Boolean transBG = pref.getBoolean("gb_transp_bg", false);
-        String sysUIMode = pref.getString("gb_sysui", "Normal");
-
-        if (sysUIMode.equals("Normal")) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
-
-        mToast = Toast.makeText(this, gbToast, Toast.LENGTH_SHORT);
         ImageView content = new ImageView(this);
-
-        if (transBG) {
-            content.setBackgroundColor(0xCA000000);
-        } else {
-            content.setBackgroundColor(0xFF000000);
-        }
-
+        content.setBackgroundColor(0xFF000000);
         content.setImageResource(R.drawable.platlogogb);
         content.setScaleType(ImageView.ScaleType.FIT_CENTER);
 
-        if (Build.VERSION.SDK_INT >= 19 && sysUIMode.equals("Immersive Mode")) {
-            content.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION        // hide nav bar
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN            // hide status bar
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);    //Immersive
-        } else if (Build.VERSION.SDK_INT >= 19 && sysUIMode.equals("Translucent Mode")) {
-            super.setTheme(style.Wallpaper_TranslucentDecor);
-        }
-
-
         setContentView(content);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        Boolean forcePort = getSharedPreferences("preferenceggs", Context.MODE_PRIVATE).getBoolean("gb_force_port", false);
-
-        if (forcePort) {
-
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        }
-
     }
 
     @Override
--- app/src/main/java/areeb/xposed/eggster/eggs/hc/PlatLogoActivity.java
+++ app/src/main/java/areeb/xposed/eggster/eggs/hc/PlatLogoActivity.java
@@ -16,74 +16,30 @@
 
 package areeb.xposed.eggster.eggs.hc;
 
-import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 import areeb.xposed.eggster.R;
 
-@SuppressLint({"ShowToast", "InlinedApi"})
 public class PlatLogoActivity extends Activity {
     Toast mToast;
     int BGCOLOR = 0xD0000000;
 
-
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SharedPreferences pref = getSharedPreferences("preferenceggs", Context.MODE_PRIVATE);
-        String SysUImode = pref.getString("hc_sysui", getString(R.string.pref_none));
-        if (SysUImode.equals("Translucent Mode")) {
-            setTheme(R.style.Wallpaper_TranslucentDecor);
-        }
         super.onCreate(savedInstanceState);
 
         getWindow().getDecorView().setBackgroundColor(BGCOLOR);
 
-        String hcToast = pref.getString("hc_toast_text", getString(R.string.pref_default_hc_text));
-        mToast = Toast.makeText(this, hcToast, Toast.LENGTH_SHORT);
-
-        if (SysUImode.equals(getString(R.string.pref_none)))
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mToast = Toast.makeText(this, "REZZZZZZZ...", Toast.LENGTH_SHORT);
 
         ImageView content = new ImageView(this);
 
         content.setImageResource(R.drawable.platlogo_hc);
         content.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-        if (Build.VERSION.SDK_INT >= 19 && SysUImode.equals("Immersive Mode")) {
-            content.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
         setContentView(content);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        Boolean forcePort = getSharedPreferences("preferenceggs", Context.MODE_PRIVATE).getBoolean("hc_force_port", false);
-
-        if (forcePort == true) {
-
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        }
-
-
     }
 
     @Override

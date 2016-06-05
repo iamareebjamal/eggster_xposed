7db3d8330fc8995ff9791f820557466d57e0c1cb
iamareebjamal <jamal.areeb@gmail.com>
2016-06-05 22:52:41 +0530

    Added setters in XPreferenceManager

--- app/src/main/java/areeb/xposed/eggster/XPreferenceManager.java
+++ app/src/main/java/areeb/xposed/eggster/XPreferenceManager.java
@@ -6,15 +6,25 @@ public class XPreferenceManager {
 
     private static XSharedPreferences preferences = new XSharedPreferences(EggsPoached.PACKAGE_NAME, "easter_preference");
     private static final String EGG_NAME = "egg_name";
+    private static final String ENABLED = "enabled";
+    private static final String LOGGING = "log";
 
     public static boolean isEnabled() {
         preferences.reload();
-        return preferences.getBoolean("enabled", true);
+        return preferences.getBoolean(ENABLED, true);
+    }
+
+    public static void setEnabled(boolean enabled){
+        preferences.edit().putBoolean(ENABLED, enabled).commit();
     }
 
     public static boolean isLoggingEnable() {
         preferences.reload();
-        return preferences.getBoolean("log", false);
+        return preferences.getBoolean(LOGGING, false);
+    }
+
+    public static void setLoggingEnabled(boolean enabled){
+        preferences.edit().putBoolean(LOGGING, enabled).commit();
     }
 
     public static String getEasterEgg() {

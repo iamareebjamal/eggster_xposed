d444decce4e37a1f6dc504c70e35aa4db6a2b95e
iamareebjamal <jamal.areeb@gmail.com>
2016-06-05 22:40:04 +0530

    Changed select mode to single

--- app/src/main/java/areeb/xposed/eggster/ui/list/EggAdapter.java
+++ app/src/main/java/areeb/xposed/eggster/ui/list/EggAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.os.Build;
+import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
@@ -23,7 +24,8 @@ public class EggAdapter extends ArrayAdapter<Egg> {
     private Context context;
     private ArrayList<Egg> eggs;
 
-    private boolean color = false;
+    private int selected = -1;
+
 
     public EggAdapter(Context context, ArrayList<Egg> eggs) {
         super(context, 0, eggs);
@@ -32,22 +34,20 @@ public class EggAdapter extends ArrayAdapter<Egg> {
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
         final Egg egg = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null)
             convertView = LayoutInflater.from(context).inflate(R.layout.item_easter_egg, parent, false);
 
-        final SwitchCompat switchCompat = (SwitchCompat) convertView.findViewById(R.id.egg_select);
+        switchHandler(convertView, position);
+        itemHandler(convertView, egg);
 
-        convertView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switchCompat.toggle();
-            }
-        });
+        return convertView;
+    }
 
+    private void itemHandler(View convertView, final Egg egg){
         final TextView textView = (TextView) convertView.findViewById(R.id.egg_title);
         final ImageView imageView = (ImageView) convertView.findViewById(R.id.egg_image);
 
@@ -76,16 +76,43 @@ public class EggAdapter extends ArrayAdapter<Egg> {
                                 egg.getPackage()));
             }
         });
+    }
 
-        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+    private void switchHandler(View convertView, final int position){
+        final SwitchCompat switchCompat = (SwitchCompat) convertView.findViewById(R.id.egg_select);
+
+        CompoundButton.OnCheckedChangeListener mListener = new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                color = checked;
 
-                notifyDataSetChanged();
+                if(checked)
+                    selected = position;
+                else
+                    selected = -1;
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable(){
+                    public void run() {
+                        notifyDataSetChanged();
+                    }
+                }, 200);
+
+            }
+        };
+
+        switchCompat.setOnCheckedChangeListener(null);
+        if(selected == position){
+            switchCompat.setChecked(true);
+        } else {
+            switchCompat.setChecked(false);
+        }
+        switchCompat.setOnCheckedChangeListener(mListener);
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchCompat.toggle();
             }
         });
-
-        return convertView;
     }
 }
